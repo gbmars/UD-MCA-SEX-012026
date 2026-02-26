079992e4388f3ca684f122f48c1ace22a817dbd8
jnndalessandro-jpg <jnndalessandro@gmail.com>
2026-02-26 16:59:13 -0300

    Update part2.js

--- aula2_20022026/part2.js
+++ aula2_20022026/part2.js
@@ -1,1 +1,22 @@
-file 2
+let meu_obj = {
+    nome = 'Máscara de Cílios',
+    cor = 'Preto',
+    preço = 'R$ 99,90',
+    estoque = '72'
+};
+
+//Como acessar nome do objeto?
+
+    console.log(meu_obj.nome);
+
+//Como acessar o preço usando colchetes?
+
+    console.log(meu_obj["preço"]);
+
+//Atualize o estoque para 80
+
+    meu_obj.estoque = 80;
+
+//Imprima todas as propriedades no console
+
+    console.log(meu_obj);
